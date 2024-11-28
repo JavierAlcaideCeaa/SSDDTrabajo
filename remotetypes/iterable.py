ef4c21e06a0ccca9431bd885d0e9067949799f39
Javier <javier.alcaide1@alu.uclm.es>
2024-11-28 19:06:07 +0100

    calidad de codigo v1

--- remotetypes/iterable.py
+++ remotetypes/iterable.py
@@ -1,15 +1,20 @@
-# iterable.py
-import RemoteTypes as rt
+"""Module for the Iterable class implementation."""
+
 from typing import Optional
 import Ice
+import RemoteTypes as rt
 
 class Iterable(rt.Iterable):
+    """Implementation of the Iterable interface."""
+
     def __init__(self, data) -> None:
+        """Initialize the Iterable with data."""
         self._data = data
         self._index = 0
         self._hash = hash(tuple(data))
 
     def next(self, current: Optional[Ice.Current] = None) -> str:
+        """Return the next item in the Iterable."""
         if self._hash != hash(tuple(self._data)):
             raise rt.CancelIteration()
         if self._index >= len(self._data):
