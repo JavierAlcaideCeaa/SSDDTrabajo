2a641bd1ec81bbd3549893962a15d10f36445995
Javier <javier.alcaide1@alu.uclm.es>
2024-12-01 00:30:53 +0100

    prueba test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,1 @@
 """Initialization module for the tests package."""
-
-import os
-import Ice
-
-try:
-    import remotetypes_ice  # noqa: F401
-except ImportError:
-    slice_path = os.path.join(
-        os.path.dirname(__file__),
-        "../remotetypes/remotetypes.ice",
-    )
-    Ice.loadSlice(f"-I{os.path.dirname(slice_path)} {slice_path}")
-    import remotetypes_ice  # noqa: F401
